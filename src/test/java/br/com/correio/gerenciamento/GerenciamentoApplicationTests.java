95b02e70d6ffa27f01b15c82296657aeb3399de0
NicollasBS <nicollasbs@outlook.com>
2024-09-16 14:34:29 -0300

    Primeiro Commit

---
+++ src/test/java/br/com/correio/gerenciamento/GerenciamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.correio.gerenciamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
