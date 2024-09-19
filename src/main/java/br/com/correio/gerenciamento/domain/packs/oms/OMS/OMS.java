4a1ab924054c6f854b6d98b41c0a5e303bb60524
NicollasBS <nicollasbs@outlook.com>
2024-09-19 11:56:32 -0300

    Alteração do enum das OMS, incluindo SFPC, ALMOX, SVP e PARTICULAR

--- src/main/java/br/com/correio/gerenciamento/domain/packs/oms/OMS/OMS.java
+++ src/main/java/br/com/correio/gerenciamento/domain/packs/oms/OMS/OMS.java
@@ -2,7 +2,10 @@ package br.com.correio.gerenciamento.domain.packs.oms.OMS;
 
 public enum OMS {
     BASE,
+    SFPC,
+    ALMOX,
     CIAPE,
+    SVP,
     CMDO5DE,
     CMDO5RM,
     CMDOAD5,
@@ -10,5 +13,6 @@ public enum OMS {
     CRO5,
     BCOM5,
     OUTROS,
+    PARTICULAR,
     DESCONHECIDO
 }
