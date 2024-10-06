1bfe78837edc575ea5c4f4fb408aa39cfe8fcfda
NicollasBS <nicollasbs@outlook.com>
2024-10-06 16:46:48 -0300

    atualização na versão

--- src/main/java/br/com/correio/gerenciamento/infra/WebConfig.java
+++ src/main/java/br/com/correio/gerenciamento/infra/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5501") // ou o domínio do seu frontend
+                .allowedOrigins("*") // ou o domínio do seu frontend
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
